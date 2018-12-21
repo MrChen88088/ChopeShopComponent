660041965a307f484a4f8fec30dab1c689fb4e7c
kevin <kevin.chen@chope.co>
2018-12-21 16:50:12 +0800

    1.project install 2.git install

---
+++ ChopeShopComponent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChopeShopComponent
+//
+//  Created by kevin on 2018/12/21.
+//  Copyright © 2018年 LEIJING.CHEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChopeShopComponent/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ChopeShopComponent
+//
+//  Created by kevin on 2018/12/21.
+//  Copyright © 2018年 LEIJING.CHEN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
